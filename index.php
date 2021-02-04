f66a50801882daae4a791aaab8c46a41cfe33a51
Blezyl Santos <blezylsantos@yahoo.com>
2021-02-04 14:19:00 -0500

    Updated route

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
 <?php
+
+//Turn in error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// require the autoload file
+require_once('vendor/autoload.php');
+// create an instance of the Base class
+$f3 = Base::instance();
+$f3->set('DEBUG', 3);
+
+// define a default route (home page)
+$f3->route('GET /', function () {
+    echo "Home Page";
+    //$view = new Template();
+    //echo $view->render("views/home.html");
+});
+
+// run fat free
+$f3->run();
